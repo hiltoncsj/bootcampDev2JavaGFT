351f1299bd194cd6295533dd80e02cff646aa059
hiltoncsj <hiltoncsj@gmail.com>
2021-06-26 13:16:58 -0300

    adding mapper and service, modifying controller and dto

---
+++ ProjetoAPIRestComSpringBoot/personapi/src/main/java/one/digitalinnovation/personapi/mapper/PersonMapperImpl.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personapi.mapper;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+public class PersonMapperImpl implements PersonMapper {
+    @Override
+    public Person toModel(PersonDTO personDTO) {
+        return null;
+    }
+
+    @Override
+    public PersonDTO toDTO(Person person) {
+        return null;
+    }
+}
--- ProjetoAPIRestComSpringBoot/personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ ProjetoAPIRestComSpringBoot/personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,7 +1,10 @@
 package one.digitalinnovation.personapi.service;
 
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class PersonService {
 
     private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
@@ -18,8 +22,11 @@ public class PersonService {
     }
 
     @PostMapping
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save( person );
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save( personToSave );
         return MessageResponseDTO.builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
