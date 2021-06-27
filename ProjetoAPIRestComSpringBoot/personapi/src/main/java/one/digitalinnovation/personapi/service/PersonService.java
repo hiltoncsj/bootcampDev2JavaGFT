242c8ca055b1d6304f337d0abf5bcb4b55f67b00
hiltoncsj <hiltoncsj@gmail.com>
2021-06-27 00:29:27 -0300

    Adding README.md and code improvement modifications

--- ProjetoAPIRestComSpringBoot/personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ ProjetoAPIRestComSpringBoot/personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -16,15 +16,11 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
