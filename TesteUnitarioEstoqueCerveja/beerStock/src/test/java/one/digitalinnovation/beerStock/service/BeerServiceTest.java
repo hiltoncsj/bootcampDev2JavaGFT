5f29a5751aa1772775f9b9f449050b6a07846926
hiltoncsj <hiltoncsj@gmail.com>
2021-07-02 20:42:00 -0300

    When beer informed then it should be created

--- TesteUnitarioEstoqueCerveja/beerStock/src/test/java/one/digitalinnovation/beerStock/service/BeerServiceTest.java
+++ TesteUnitarioEstoqueCerveja/beerStock/src/test/java/one/digitalinnovation/beerStock/service/BeerServiceTest.java
@@ -1,10 +1,53 @@
 package one.digitalinnovation.beerStock.service;
 
+import one.digitalinnovation.beerStock.builder.BeerDTOBuilder;
+import one.digitalinnovation.beerStock.dto.BeerDTO;
+import one.digitalinnovation.beerStock.entity.Beer;
+import one.digitalinnovation.beerStock.exception.BeerAlreadyRegisteredException;
+import one.digitalinnovation.beerStock.mapper.BeerMapper;
+import one.digitalinnovation.beerStock.repository.BeerRepository;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerServiceTest {
 
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    @Test
+    void whenBeerInformedThenItShouldBeCreated() throws BeerAlreadyRegisteredException {
+        // given
+        BeerDTO expectedBeerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedSavedBeer = beerMapper.toModel(expectedBeerDTO);
+
+        // when
+        when(beerRepository.findByName(expectedBeerDTO.getName())).thenReturn(Optional.empty());
+        when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
+
+        //then
+        BeerDTO createdBeerDTO = beerService.createBeer(expectedBeerDTO);
+
+        assertThat(createdBeerDTO.getId(), is(equalTo(expectedBeerDTO.getId())));
+        assertThat(createdBeerDTO.getName(), is(equalTo(expectedBeerDTO.getName())));
+        assertThat(createdBeerDTO.getQuantity(), is(equalTo(expectedBeerDTO.getQuantity())));
+    }
 
 }
